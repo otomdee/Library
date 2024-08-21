314042bc7eae0f9a9ffd056288644e6d8d442928
otomdee <otomdaniel65@gmail.com>
2024-08-21 14:41:43 +0100

    set up files

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(Book) {
+    myLibrary.push(Book);
+}
+
+// Get selectors//
+
+const div = 
