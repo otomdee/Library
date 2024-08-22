1d09c5f8ab547796b33bba3a54f4e33471c97759
otomdee <otomdaniel65@gmail.com>
2024-08-22 10:16:00 +0100

    add remove book button, add change read status button

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const myLibrary = [];
+let index = 0;
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.index = 0;
 }
 
 function addBookToLibrary(Book) {
     myLibrary.push(Book);
+    index++;
+    Book.index = index;
 }
 
 // Get container selector//
@@ -22,32 +26,55 @@ function addBookToDOM(book) {
     const cardAuthor = document.createElement("span");
     const cardPages = document.createElement("span");
     const cardRead = document.createElement("span");
+    const cardReadChange = document.createElement("button");
+    const cardRemove = document.createElement("button");
 
     //populate card//
     cardTitle.innerHTML = `Title: ${book.title}`;
     cardAuthor.innerHTML = `Author: ${book.author}`;
     cardPages.innerHTML = `Pages: ${book.pages}`;
+    cardReadChange.innerHTML = "Change Status";
     if (book.read === true) {
-        cardRead.innerHTML = "Current Status: Read";
+        cardRead.innerHTML = "Status: Read";
     }
     else {
-        cardRead.innerHTML = "Current Status: Not read";
+        cardRead.innerHTML = "Status: Not read";
     }
+    cardRemove.innerHTML = "Remove";
 
     //add items to card//
     card.append(cardTitle);
     card.append(cardAuthor);
     card.append(cardPages);
     card.append(cardRead);
+    card.append(cardReadChange);
+    card.append(cardRemove);
+    //removeBook button
+    cardRemove.classList.add("removeBtn");
+    cardRemove.addEventListener("click", () => {
+        myLibrary.splice((book.index - 1), 1);
+        card.remove();
+    })
+    //changeReadStatus button
+    cardReadChange.classList.add("changeReadBtn");
+    cardReadChange.addEventListener("click", () => {
+        if (book.read === true) {
+            book.read = false;
+            cardRead.innerHTML = "Status: Not read";
+        }
+        else {
+            book.read = true;
+            cardRead.innerHTML = "Status: Read";
+        }
+    })
 
     card.classList.add("bookCard");
     return card;
 }
 
 //dummy books//
-addBookToLibrary(new Book("green", "mayd", 13, true));
+addBookToLibrary(new Book("Harry Potter", "J. K. Rowling", 500, true));
 addBookToLibrary(new Book("blue", "ray", 156, false));
-
 
 myLibrary.forEach((item) => {
     container.append(addBookToDOM(item));
